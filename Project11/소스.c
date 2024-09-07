b49d382da7dcb16858e35c7e08d2b9cfc9f8a3f2
hibaek00 <qorwhdqh1245@gmail.com>
2024-09-07 12:17:55 +0900

    init

---
+++ Project11/소스.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+
+int main()
+{
+	int num, i, j;
+
+	printf("문자열 길이 입력: ");
+	scanf("%d", &num);
+
+	char* arr = (char*)malloc(sizeof(char) * (num + 1));
+	
+	getchar();
+	
+	printf("문자열 입력: ");
+	
+	fgets(arr, sizeof(arr), stdin);
+
+	printf("%c", arr[2]);
+
+	/*for (i = 0; i < num; i++)
+	{
+		if (arr[i] == ' ')
+		{
+			for (j = i - 1; arr[j] != ' '; j--)
+			{
+				printf("%c", arr[j]);
+			}
+			printf(" ");
+		}
+	}*/
+}
